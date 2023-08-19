173485e51e4f809c0f1fa4cd4621d7d0eac72ce3
yuliia <pustovayajulia@gmail.com>
2023-08-19 18:48:30 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+arr = {};
+// fetch(
+//     `https://www.googleapis.com/books/v1/volumes?q=search-terms&key=AIzaSyD-bBO1Xn8SCKvOQ3v2QjRsrHD2FwilA9M`)
+// .then(response => response.json())
+// .then(result => {
+//  let a= { books: result.items};
+//  arr.push(a)
+// }).then(()=>console.log("!"+arr));
+
+//volume zadan
+// fetch(`https://www.googleapis.com/books/v1/volumes/zyTCAlFPjgYC?key=AIzaSyD-bBO1Xn8SCKvOQ3v2QjRsrHD2FwilA9M`)
+// .then(response=>response.json())
+// .then(res=> console.log(res))
+
